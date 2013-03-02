000b4020696a57d4331f5c2bf6daa183246bda2c
John Ford <john@johnford.info>
2013-03-01 16:30:32 -0800

    move main function logic to the driver module

--- uplift.py
+++ uplift.py
@@ -15,10 +15,6 @@ import util
 import find_commits
 import reporting
 
-gaia_path = os.path.abspath(os.path.join(os.getcwd(), 'uplift-gaia'))
-gaia_url = "github.com:mozilla-b2g/gaia.git"
-query_file = os.path.abspath("uplift_queries.dat")
-
 
 # Should be smarter about these cache files and either manage them in sets
 # or use a single file which contains *all* the information only ever added to
@@ -149,34 +145,4 @@ def build_uplift_requirements(repo_dir, queries):
             b['summary'] = bug['summary']
         write_cache_file(bug_info, requirements_cache_file)
     return bug_info
-
-
-if __name__ == "__main__":
-    with open(query_file, 'rb') as f:
-        queries = [x.strip() for x in f.readlines()]
-
-    if len(sys.argv) < 2:
-        print "You must specify a command"
-        exit(1)
-
-    cmd = sys.argv[1]
-    cmd_args = sys.argv[1:]
-
-    if cmd == 'show':
-        bugs = build_uplift_requirements(gaia_path, queries)
-        print "\n\nRequirements for Bug uplift:"
-        print reporting.display_uplift_requirements(bugs)
-    elif cmd == 'uplift':
-        requirements = build_uplift_requirements(gaia_path, queries)
-        print "\n\nUplift requirements:"
-        print reporting.display_uplift_requirements(requirements)
-        uplift_report = uplift(gaia_path, requirements)
-        print reporting.display_uplift_report(uplift_report)
-        print reporting.display_uplift_comments(gaia_path, uplift_report)
-
-
-
-
-
-
 
