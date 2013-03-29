ba1b58c07a920348c18308c29093d213594e1c84
John Ford <john@johnford.info>
2013-03-29 14:25:14 -0700

    change around some branch logic with respect to rules and branches

--- gaia_uplift/branch_logic.py
+++ gaia_uplift/branch_logic.py
@@ -23,35 +23,38 @@ def flags_to_set(branches):
 
 def fixed_on_branches(bug):
     b = []
-    if bug['cf_status_b2g18'] == 'fixed':
+    if bug.get('cf_status_b2g18') == 'fixed':
         b.append('v1-train')
-    if bug['cf_status_b2g18_1_0_0'] == 'fixed':
+    if bug.get('cf_status_b2g18_1_0_0') == 'fixed':
         b.append('v1.0.0')
-    if bug['cf_status_b2g18_1_0_1'] == 'fixed':
+    if bug.get('cf_status_b2g18_1_0_1') == 'fixed':
         b.append('v1.0.1')
-    #if bug['cf_status_b2g18_1_1_0'] == 'fixed':
-    #    b.append('v1.1.0')
+    if bug.get('cf_status_b2g18_1_1_0') == 'fixed':
+        b.append('v1.1.0')
     return b
 
 
 def needed_on_branches(bug):
+    needed_on = []
+    fixed_on = fixed_on_branches(bug)
+
+    def _a(x):
+        if not x in needed_on and not x in fixed_on and x in branches:
+            needed_on.append(x)
+
     blocking_b2g = bug['cf_blocking_b2g']
-    tracking_b2g18 = bug['cf_tracking_b2g18'] == "+"
 
     if blocking_b2g == 'tef+' or blocking_b2g == 'shira+':
-        return ['v1-train', 'v1.0.1']
+        _a('v1-train')
+        _a('v1.0.1')
     elif blocking_b2g == 'leo+':
-        # Until we have a real v1.1.0 branch
-        #return ['v1-train', 'v1.1.0']
-        return ['v1-train']
-    elif tracking_b2g18:
-        return ['v1-train']
+        _a('v1-train')
     else:
         for a in bug['attachments']:
             for f in a.get('flags', []):
                 if f['name'] == 'approval-gaia-v1' and f['status'] == '+':
-                    return ['v1-train']
-    return []
+                    needed_on.append('v1-train')
+    return needed_on
 
 def determine_branches(bug_id):
     bug = bzapi.fetch_bug(bug_id)
