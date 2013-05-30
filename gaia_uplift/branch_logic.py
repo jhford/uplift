93e6802bbeae2345f9ca516533b1f1143d84594b
John Ford <john@johnford.info>
2013-05-30 13:36:12 -0700

    support the v1.1hd branch

--- gaia_uplift/branch_logic.py
+++ gaia_uplift/branch_logic.py
@@ -1,14 +1,15 @@
-""" This module understands a which bugs related to which branches """
 import bzapi
 
-#branches = ['v1-train', 'v1.1.0', 'v1.0.1', 'v1.0.0']
-branches = ['v1-train', 'v1.0.1']
+branches = ['v1-train', 'v1.1hd', 'v1.0.1']
 
 def flags_to_set(branches):
     f = {}
     for b in branches:
         if b == 'v1-train' and b in branches:
             f['cf_status_b2g18'] = 'fixed'
+        # TODO: Need to verify if this is the correct flag!
+        elif b == 'v1.1hd' and b in branches:
+            f['cf_status_b2g18_1_1_hd'] = 'fixed'
         elif b == 'v1.0.0' and b in branches:
             f['cf_status_b2g18_1_0_0'] = 'fixed'
         elif b == 'v1.0.1' and b in branches:
@@ -22,6 +23,9 @@ def fixed_on_branches(bug):
     b = []
     if bug.get('cf_status_b2g18') == 'fixed':
         b.append('v1-train')
+    # TODO: Need to verify if this is the correct flag!
+    if bug.get('cf_status_b2g18_1_1_hd') == 'fixed':
+        b.append('v1.1hd')
     if bug.get('cf_status_b2g18_1_0_0') == 'fixed':
         b.append('v1.0.0')
     if bug.get('cf_status_b2g18_1_0_1') == 'fixed':
@@ -43,13 +47,16 @@ def needed_on_branches(bug):
 
     if blocking_b2g == 'tef+' or blocking_b2g == 'shira+':
         _a('v1-train')
+        _a('v1.1hd')
         _a('v1.0.1')
     elif blocking_b2g == 'leo+':
         _a('v1-train')
+        _a('v1.1hd')
     else:
         for a in bug['attachments']:
             for f in a.get('flags', []):
                 if f['name'] == 'approval-gaia-v1' and f['status'] == '+':
-                    needed_on.append('v1-train')
+                    _a('v1-train')
+                    _a('v1.1hd')
     return needed_on
 
