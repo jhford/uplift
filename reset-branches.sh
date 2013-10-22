9ecd494fee50edb0ae3c8979ea08e12c1328be27
John Ford <john@johnford.info>
2013-10-22 14:55:24 -0700

    fix reset-branches.sh

--- reset-branches.sh
+++ reset-branches.sh
@@ -2,7 +2,7 @@
 
 cd gaia
 git checkout -b temp
-for b in v1.0.1 v1-train v1.1.0hd master ; do
+for b in v1.2 v1-train v1.1.0hd master ; do
     git reset --hard HEAD
     git branch -D $b
     git checkout -t origin/$b -b $b
