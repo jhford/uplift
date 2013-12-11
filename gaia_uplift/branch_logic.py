3b2721cc197b14aeeb7be290ba483308f0a83722
John Ford <john@johnford.info>
2013-12-10 16:05:20 -0800

    make this file data driven

--- gaia_uplift/branch_logic.py
+++ gaia_uplift/branch_logic.py
@@ -3,43 +3,43 @@ import bzapi
 
 branches = ['v1.2', 'v1-train', 'v1.1.0hd'] #, 'v1.0.1']
 
+status_flags = {
+    'v1.3': 'cf_status_b2g_1_3',
+    'v1.2': 'cf_status_b2g_1_2',
+    'v1-train': 'cf_status_b2g18',
+    'v1.0.1': 'cf_status_b2g18_1_0_1',
+    'v1.0.0': 'cf_status_b2g18_1_0_0',
+    'v1.1.0hd': 'cf_status_b2g_1_1_hd'
+}
+
+blocking_rules = {
+    'v1.3+': ['v1.3'],
+    'koi+': ['v1.3', 'v1.2'],
+    'leo+': ['v1.3', 'v1.2', 'v1-train'],
+    'tef+': ['v1.3', 'v1.2', 'v1-train', 'v1.0.1'],
+    'shira+': ['v1.3', 'v1.2', 'v1-train', 'v1.0.1'],
+}
+
+patch_rules = {
+    'approval-gaia-v1': ['v1.3', 'v1.2', 'v1-train'],
+    'approval-gaia-v1.2': ['v1.3', 'v1.2'],
+}
+
 def flags_to_set(for_branches):
     fb = []
     for b in [x for x in for_branches if x in branches]:
-        if b == 'v1.3':
-            fb.append('cf_status_b2g_1_3')
-        elif b == 'v1.2':
-            fb.append('cf_status_b2g_1_2')
-        elif b == 'v1-train':
-            fb.append('cf_status_b2g18')
-        elif b == 'v1.0.0':
-            fb.append('cf_status_b2g18_1_0_0')
-        elif b == 'v1.0.1':
-            fb.append('cf_status_b2g18_1_0_1')
-        elif b == 'v1.1.0':
-            fb.append('cf_status_b2g18_1_1_0')
-        elif b == 'v1.1.0hd':
-            fb.append('cf_status_b2g_1_1_hd')
+        if b in status_flags.keys():
+            fb.append(status_flags[b])
     return dict([(x, 'fixed') for x in fb])
 
 
 def fixed_on_branches(bug):
+    branches = dict([(status_flags[k],k) for k in status_flags.keys()])
     b = []
     for x in ('fixed', 'verified'):
-        if bug.get('cf_status_b2g_1_3') == x:
-            b.append('v1.3')
-        if bug.get('cf_status_b2g_1_2') == x:
-            b.append('v1.2')
-        if bug.get('cf_status_b2g18') == x:
-            b.append('v1-train')
-        if bug.get('cf_status_b2g18_1_0_0') == x:
-            b.append('v1.0.0')
-        if bug.get('cf_status_b2g18_1_0_1') == x:
-            b.append('v1.0.1')
-        if bug.get('cf_status_b2g18_1_1_0') == x:
-            b.append('v1.1.0')
-        if bug.get('cf_status_b2g_1_1_hd') == x:
-            b.append('v1.1.0hd')
+        for flag in branches.keys():
+            if bug.get(flag) and not branches[flag] in b:
+                b.append(branches[flag])
     return b
 
 
@@ -48,34 +48,17 @@ def needed_on_branches(bug):
     fixed_on = fixed_on_branches(bug)
 
     def _a(x):
-        if not x in needed_on and not x in fixed_on and x in branches:
-            needed_on.append(x)
+        for y in x:
+            if not y in needed_on and not y in fixed_on and y in branches:
+                needed_on.append(y)
 
-    blocking_b2g = bug['cf_blocking_b2g']
+    _a(blocking_rules[bug['cf_blocking_b2g']])
 
-    if blocking_b2g == 'v1.3+':
-        _a('v1.3')
-    elif blocking_b2g == 'koi+':
-        _a('v1.3')
-        _a('v1.2')
-    elif blocking_b2g == 'leo+':
-        _a('v1.3')
-        _a('v1.2')
-        _a('v1-train')
-    elif blocking_b2g == 'tef+' or blocking_b2g == 'shira+':
-        _a('v1.3')
-        _a('v1.2')
-        _a('v1-train')
-        _a('v1.0.1')
-    else:
-        for a in bug['attachments']:
+    for flag in patch_rules.keys():
+        for a in bug.get('attachments', []):
             for f in a.get('flags', []):
-                if f['name'] == 'approval-gaia-v1' and f['status'] == '+':
-                    _a('v1.3')
-                    _a('v1.2')
-                    _a('v1-train')
-                elif f['name'] == 'approval-gaia-v1.2' and f['status'] == '+':
-                    _a('v1.3')
-                    _a('v1.2')
+                if f['name'] == flag and f['status'] == '+':
+                    _a(patch_rules[flag])
+
     return needed_on
 
